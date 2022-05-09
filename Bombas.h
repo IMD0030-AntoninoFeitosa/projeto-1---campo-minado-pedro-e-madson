c0b4a2e67ac7cc01410af5f87173dca193ce2e2a
Mádson Douglas <santos.madson94@gmail.com>
2022-05-09 16:59:02 -0300

    Update Bombas.h

--- Bombas.h
+++ Bombas.h
@@ -1,24 +1,18 @@
-
-// TODO devem estar dentro do ifndef
-#include <cstdlib> 
+#include <cstdlib>
 #include <ctime>
 #include <iostream>
 
 #include "Game.h"
 #include "Jogada.h"
 #include <vector>
-// TODO tudo isso
 
-
-#ifndef BOMBAS_H // TODO deve conter todas as declarações do arquivo incluindo a inclusão das bibliotecas acima
+#ifndef BOMBAS_H
 #define BOMBAS_H
 
 std::vector<int> gerar_indices(cenario cena);
-cenario preencher_bombas(cenario cena);
+void preencher_bombas(cenario& cena);
 void print_bombas(cenario cena);
 bool verifica_bomba(cenario cena, std::vector<int> celula);
-
-// TODO as implementações das funções devem ficar num arquivo .cpp
 
 std::vector<int> gerar_indices(cenario cena) {
   srand((unsigned)time(0));
@@ -33,31 +27,18 @@ std::vector<int> gerar_indices(cenario cena) {
   return indices;
 }
 
-cenario preencher_bombas(cenario cena) {
-  // TODO é mais eficiente verificar uma mina de cada vez ao invés de verificar se o mapa é válido
-
-  do {
-    // gerando indices aleatorios
-    std::vector<int> indices = gerar_indices(cena);
-
-    // verificar se indices já existem
-    bool existe = false;
-    for (int i = 0; i < cena.bombas.size(); i++) {
+void preencher_bombas(cenario& cena) {
+    int count_bombas = 0;
+    do {
+      std::vector<int> indices = gerar_indices(cena);
       int l = indices[0];
       int c = indices[1];
-      if (cena.bombas[i][0] == l && cena.bombas[i][1] == c) {
-        existe = true;
-        break;
+    
+      if (cena.mapa[l][c] != '@'){
+        cena.mapa[l][c] = '@';
+        count_bombas++;
       }
-    }
-
-    if (!existe) {
-      cena.bombas.push_back(indices);
-    }
-
-  } while (cena.bombas.size() < cena.dimensoes.minas);
-
-  return cena;
+    } while(count_bombas < cena.dimensoes.minas);
 }
 
 void print_bombas(cenario cena) {
@@ -72,13 +53,9 @@ void print_bombas(cenario cena) {
 bool verifica_bomba(cenario cena, std::vector<int> celula) {
   int l = celula[0];
   int c = celula[1];
-
-  for (int i = 0; i < cena.bombas.size(); i++) {
-    if (cena.bombas[i][0] == l && cena.bombas[i][1] == c) {
-      return true;
-    }
+  if (cena.mapa[l][c] == '@') {
+    return true;
   }
-
   return false;
 }
 
