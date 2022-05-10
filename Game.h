1ef3df94103a56c1107fd6df53891a82c0388cad
Pedro Cantidio <ppcantidio@gmail.com>
2022-05-10 10:31:04 -0300

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,15 +1,25 @@
-#include <vector>
-
 #ifndef GAME_H
 #define GAME_H
 
+#include <iostream>
+#include <array>
+#include <cctype>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <chrono>
+
 enum Difficulty { beginner, intermediary, advanced };
+enum Opcoes { iniciar_jogo = 1, creditos_jogo, instrucoes_jogo, sair_jogo };
 
 struct cenario {
   std::vector<std::vector<char>> mapa;
   std::vector<std::vector<int>> bombas;
   std::vector<std::vector<int>> lados_adjacentes; 
-  std::vector<std::vector<int>> marcacoes; 
+  int reveladas;
+  int marcacoes; 
+  int quantidade_jogadas;
   std::vector<std::vector<int>> selecionados; // vetor com todos as celulas que o usuario já clicou
 
   struct {
@@ -19,5 +29,10 @@ struct cenario {
   } dimensoes;
 
 };
+
+void store_difficulty(const std::string config_file, Difficulty level);
+Difficulty load_difficulty(const std::string config_file);
+void print_mapa(cenario cena, std::chrono::time_point<std::chrono::high_resolution_clock> start);
+int contar_bombas(cenario cena, std::vector<int> celula);
 
 #endif
