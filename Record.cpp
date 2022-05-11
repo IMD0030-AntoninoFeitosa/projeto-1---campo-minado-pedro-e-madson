140eb87c504623f323a1b89d33fb380c343bf5e2
Mádson Douglas <santos.madson94@gmail.com>
2022-05-11 10:39:45 -0300

    Update Record.cpp

--- Record.cpp
+++ Record.cpp
@@ -47,10 +47,49 @@ void sort(std::vector<Record> &records) {
   std::sort(records.begin(), records.end(), record_cmp);
 }
 
+std::vector<Record> ranking_nivel (std::vector<Record> &records, int nivel){
+  std::vector<Record> saida;
+  for (int i = 0; i < records.size(); i++){
+    if(records[i].nivel == nivel){
+      saida.push_back(records[i]);
+    }
+  }
+  return saida;
+}
+void print_ranking(std::vector<Record> &records) {
+  
+  std::vector<Record> iniciante = ranking_nivel(records, 0);
+  std::vector<Record> intermediario = ranking_nivel(records, 1);
+  std::vector<Record> avancado = ranking_nivel(records, 2);
+  
+  std::sort(iniciante.begin(), iniciante.end(), record_cmp);
+  std::sort(intermediario.begin(), intermediario.end(), record_cmp);
+  std::sort(avancado.begin(), avancado.end(), record_cmp);
+
+  std::cout << "\033[1;32m---------- MELHORES PONTUAÇÕES-------------"<<std::endl;
+  if (iniciante.size() > 0){
+    std::cout <<"INICIANTE     "<<std::endl;
+    std::cout <<"              NOME: " << iniciante[0].name <<std::endl;
+    std::cout <<"              TEMPO: " << iniciante[0].milliseconds << "/s"<< std::endl;
+  }
+  if (intermediario.size() > 0){
+    std::cout <<"INTERMEDIARIO "<<std::endl;
+    std::cout <<"              NOME: " << intermediario[0].name <<std::endl;
+    std::cout <<"              TEMPO: " << intermediario[0].milliseconds << "/s"<< std::endl;
+  }
+  if (avancado.size() > 0){
+    std::cout <<"AVANÇADO      "<<std::endl;
+    std::cout <<"              NOME: " << avancado[0].name <<std::endl;
+    std::cout <<"              TEMPO: " << avancado[0].milliseconds << "/s"<< std::endl;
+  }
+
+  std::cout << "--------------------------------------------\033[0m"<<std::endl;
+}
+
 void write_records(std::vector<Record> &records, std::string &file_name) {
 
   std::ofstream file;
-  file.open(file_name.c_str());
+  file.open(file_name.c_str(), std::ios::app);
   if (file.is_open()) {
     file << records.size() << std::endl;
     for (Record rec : records) {
@@ -63,13 +102,17 @@ void write_records(std::vector<Record> &records, std::string &file_name) {
 }
 
 void read_records(std::vector<Record> &records, std::string &file_name) {
+  std::ifstream inFile(file_name); 
+  
+  int lines = std::count(std::istreambuf_iterator<char>(inFile), 
+             std::istreambuf_iterator<char>(), '\n');
   std::ifstream file;
   file.open(file_name.c_str());
   if (file.is_open()) {
     records.clear();
-    int size;
-    file >> size;
-    file.ignore();
+    int size = lines / 3;
+    // file >> size;
+    // file.ignore();
     for (int i = 0; i < size; i++) {
       Record rec;
       std::getline(file, rec.name);
