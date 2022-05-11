79e3b4b709bf191f0e50d3ad7425b066697637d0
Mádson Douglas <santos.madson94@gmail.com>
2022-05-11 09:18:40 -0300

    Create Record.hpp

---
+++ Record.hpp
@@ -0,0 +1,34 @@
+#ifndef RECORD_H_
+#define RECORD_H_
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct Record {
+  std::string name;
+  double milliseconds;
+  int nivel;
+};
+
+
+bool record_cmp(Record &a, Record &b);
+
+std::ostream &operator<<(std::ostream &os, Record const &r);
+
+std::ostream &operator<<(std::ostream &os, std::vector<Record> &vet);
+
+void sort(std::vector<Record> &records);
+
+void write_records(std::vector<Record> &records, std::string &file_name);
+
+void read_records(std::vector<Record> &records, std::string &file_name);
+
+
+void write(char * file_name, int n, Record ** vector);
+
+Record ** read(char * file_name, int & n);
+
+#endif
